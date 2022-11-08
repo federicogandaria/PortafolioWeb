f239843caa48d84b72bd3052ddf4a3d306f3ddde
federicogandaria <federicogandaria@gmail.com>
2022-11-08 00:42:27 -0300

    Primera version de portafolio web 08/11/2022

---
+++ script.js
@@ -0,0 +1,9 @@
+//función que muestra el menu responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
